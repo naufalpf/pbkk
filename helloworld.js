94f64a70f5f94f78af057a1d0a0f7f36ec714f00
Naufal <naufalpf@gmail.com>
2019-02-04 11:27:20 +0700

    hello world

--- helloworld.js
+++ helloworld.js
@@ -1,7 +1,8 @@
-const express = require('express')
-const app = express()
-const port = 1000
+var express = require('express');
+var app = express();
 
-app.get('/', (req, res) => res.send('Hello world'))
+app.get('/', function(req, res){
+   res.send("Hello world");
+});
 
-app.listen(port)
+app.listen(3000);

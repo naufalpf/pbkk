617cbb4b1f2ddb13e63391200ea33838c21b0a73
Naufal <naufalpf@gmail.com>
2019-02-04 15:08:36 +0700

    hello

--- helloworld.js
+++ helloworld.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
-var port = 3000
+var port = 3000;
 
-app.get('/', (req, res) => res.send('Hello world'))
+app.get('/', (req, res) => res.send('Hello world'));
 
 app.listen(port, function() {
-    console.log("run")
-})
+    console.log('Buka browser ke alamat pada port localhost:3000')
+});
